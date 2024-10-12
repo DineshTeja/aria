05f4c8d5056815ab170308c7b643316e94d3df36
Pranav Ramesh <pranav.ramesh1@gmail.com>
2024-10-12 13:51:26 -0400

    Fixed supabase env var issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables");
